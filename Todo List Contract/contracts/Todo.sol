d66fc65fbe0ed56e3bea332c538d9aef967a5a36
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-05-23 16:28:56 +0100

    Update Todo.sol

--- Todo List Contract/contracts/Todo.sol
+++ Todo List Contract/contracts/Todo.sol
@@ -5,17 +5,34 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Todo {
-    struct item {
-        bytes32 content;
-        address owner;
-        bool completed;
-        uint256 timestamp;
-    }
+        struct Item {
+            bytes32 content;
+            address owner;
+            bool completed;
+            uint256 timestamp;
+        }
 
-    uint public constant maximumItems = 50;
+        uint public constant maximumItems = 50;
 
-    mapping(address => item[maximumItems]) public todos;
-   
-   mapping(address => uint256) public lastIds;
-   
+        mapping(address => Item[maximumItems]) public todos;
+    
+        mapping(address => uint256) public lastIds;
+
+        modifier onlyOwner (address _owner) {
+            require(msg.sender == _owner);
+            _;
+        }
+
+        function addItem(bytes32 _content) public{
+            Item memory newItem = Item(_content, msg.sender, true, block.timestamp);
+            todos[msg.sender][lastIds[msg.sender]] = newItem;
+            if(lastIds[msg.sender] >= maximumItems) lastIds[msg.sender] = 0;
+            else lastIds[msg.sender]++;
+   }
+        
+        function markTodoAsCompleted(uint256 _todoId) public {
+            require(_todoId < maximumItems);
+            require(!todos[msg.sender][_todoId].isCompleted);
+            todos[msg.sender][_todoId].isCompleted = true;
+   }
 }
